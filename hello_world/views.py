dc034f886c1c165adf97e162897c238f5db80605
Wasim Alrawas <wassimrawwas@gmail.com>
2024-04-02 20:58:28 +0000

    add request.method function

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World From Django project!!")
+
+def index(request):
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
+       
